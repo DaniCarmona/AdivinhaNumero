c63e6aee6676d8fb5ab826b5ce5a7a4d67300f5f
Daniel Carmona <carmona240101@hotmail.com>
2021-05-07 10:02:52 +0100

    funçao novo jogo

--- app/src/main/java/pt/ipg/adivinhanumero/MainActivity.kt
+++ app/src/main/java/pt/ipg/adivinhanumero/MainActivity.kt
@@ -2,10 +2,36 @@ package pt.ipg.adivinhanumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val random = Random()
+    private var numeroAdivinhar : Int = 0
+    private var jogo : Int = 0
+    private var tentativas : Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        novoJogo()
+    }
+
+    private fun novoJogo() {
+        numeroAdivinhar = random.nextInt(10) + 1
+        tentativas = 0
+        jogo++
+        atualizaEcra()
+    }
+
+    private fun atualizaEcra() {
+
+    }
+
+
+    fun adivinha(view: View) {
+        tentativas++
     }
 }
